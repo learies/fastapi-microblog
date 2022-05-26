1581922138415760cfd0feff5b12f9c9f46bec98
LeAries <learies@ya.ru>
2022-05-26 22:37:42 +0300

    Перенес Base.metadata в get_db

--- src/main.py
+++ src/main.py
@@ -1,14 +1,11 @@
 from fastapi import FastAPI
 
-from .database import engine
-from .models import Base
 from .routers import router
 
 
 def create_app() -> FastAPI:
     app = FastAPI()
     app.include_router(router)
-    Base.metadata.create_all(bind=engine)
     return app
 
 
